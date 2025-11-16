e4feaa17c9c96c0d682a79cef37c0fc262b31924
jiyeonyooo <ji04230423@gmail.com>
2025-11-16 21:49:07 +0900

    feat: 댓글 입력/목록 UI 및 기능 구현

---
+++ src/features/comment/components/Comments.js
@@ -0,0 +1,62 @@
+import Component from "../../../core/Component.js";
+import { getAuthUser } from "../../../lib/api.js";
+import CommentInput from "./CommentInput.js";
+import CommentItem from "./CommentItem.js";
+
+export default class Comments extends Component {
+
+   template() {
+
+      const { postId, commentList } = this.props;
+
+      const frag = document.createDocumentFragment();
+
+      const $wrapper = document.createElement('div');
+      $wrapper.className = 'post-detail-comment-wrapper';
+
+      const $commentInput = document.createElement('div');
+      $commentInput.className = 'post-detail-comment-input';
+
+      const commentInputProps = {
+         $target: $commentInput,
+         postId: postId,
+      };
+
+      const commentInput = new CommentInput(commentInputProps);
+      commentInput.render();
+
+      const $commentList = document.createElement('div');
+      $commentList.className = 'post-detail-comment-list';
+
+      commentList.forEach((comment) => {
+         const commentItemProps = {
+            postId: postId,
+            commentId: comment.commentId,
+            nickname: comment.author.nickname,
+            content: comment.content,
+            updatedAt: comment.updatedAt,
+            isOwner: Number(getAuthUser().userId) === Number(comment.author.userId),
+         }
+
+         const commentItem = new CommentItem(commentItemProps).render();
+         $commentList.appendChild(commentItem);
+      })
+
+      $wrapper.append($commentInput, $commentList);
+      frag.append($wrapper);
+
+      this.$refs = { commentList: $commentList, commentInput: commentInput };
+
+      return frag;
+   }
+
+   setEvent() {
+      const { commentList, commentInput } = this.$refs;
+
+      commentList.addEventListener('comment:edit', (e) => {
+         const { id, content } = e.detail;
+         console.log(content);
+         commentInput.editMode({ id, content });
+      });
+   }
+}
