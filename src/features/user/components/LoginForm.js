edf5f6d313007d5b810442478bda8adbdd7935c5
jiyeonyooo <ji04230423@gmail.com>
2025-11-26 14:54:24 +0900

    style: 로그인 폼 디자인 수정

--- src/features/user/components/LoginForm.js
+++ src/features/user/components/LoginForm.js
@@ -6,7 +6,7 @@ import { isValidEmail, isValidPassword } from '../../../lib/auth.js';
 export default class LoginForm extends Component {
    
    setup() {
-      this.state = { isCompleted: false };
+      this.state = { isCompleted: false, isChecked: false };
    }
 
    template() {
@@ -17,9 +17,9 @@ export default class LoginForm extends Component {
       $form.className = 'login-form';
 
       const $email = document.createElement('div');
-      $email.className = 'login-form-email';
+      $email.className = 'login-form-email input';
       const $password = document.createElement('div');
-      $password.className = 'login-form-password';
+      $password.className = 'login-form-password input';
 
       const email = new CustomInput({ $target: $email, label: '이메일', name: 'email', type: 'email', placeholder: '이메일을 입력하세요.',required: true });
       const password = new CustomInput({ $target: $password, label: '비밀번호', name: 'password', type: 'password', placeholder: '비밀번호를 입력하세요', required: true });
@@ -27,22 +27,33 @@ export default class LoginForm extends Component {
       email.render();
       password.render();
 
+      const $memory = document.createElement('div');
+      $memory.className = 'login-memory-id';
+
+      const $checkbox = document.createElement('i');
+      $checkbox.className = 'fa-regular fa-square-check';
+      const $text = document.createElement('div');
+      $text.className = 'login-memory-id-text';
+      $text.textContent = '아이디 저장';
+
+      $memory.append($checkbox, $text);
+
       const $submit = document.createElement('button');
       $submit.className = 'form-submit-button';
       $submit.type = 'submit';
       $submit.textContent = '로그인'
       $submit.disabled = !this.state.isCompleted;
 
-      $form.append($email, $password, $submit);
-      frag.appendChild($form);
+      $form.append($email, $password, $memory, $submit);
+      frag.append($form);
 
-      this.$refs = { form: $form, email: email, password: password, submit: $submit, emailInput: email.$refs.input, passwordInput: password.$refs.input };
+      this.$refs = { form: $form, email: email, password: password, submit: $submit, emailInput: email.$refs.input, passwordInput: password.$refs.input, checkbox: $checkbox };
 
       return frag;
    }
 
    setEvent() {
-      const { form, email, password, submit, emailInput, passwordInput } = this.$refs;
+      const { form, email, password, submit, emailInput, passwordInput, checkbox } = this.$refs;
 
       emailInput.addEventListener('input', () => {
          const input = emailInput.value || '';
@@ -97,12 +108,9 @@ export default class LoginForm extends Component {
                withAuth: false
             });
 
-            //추후 이미지 처리 시 구현 필요 
-            const profileImg = response.data.userId === 53 ? '/img/profile_img_dog.JPG' : '/img/profile_img_ms.JPG';
-
             setAccessToken(response.data.accessToken);
             setRefreshToken(response.data.refreshToken);
-            setAuthUser({ userId: response.data.userId, profileImg: profileImg });
+            setAuthUser({ userId: response.data.userId, profileImg: response.data.profileImg });
 
             window.history.pushState({}, '', '/posts');
             window.dispatchEvent(new PopStateEvent('popstate'));
@@ -112,6 +120,12 @@ export default class LoginForm extends Component {
                password.setState({ isValidInput: false, errorText: '*아이디 또는 비밀번호를 확인해주세요'});
             }
          }
-      })
+      });
+
+      checkbox.addEventListener('click', (e) => {
+         checkbox.classList.toggle('fa-regular');
+         checkbox.classList.toggle('fa-solid');
+      });
+
    }
 }
